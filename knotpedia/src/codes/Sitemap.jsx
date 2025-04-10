76dc8f03bf64b2e458c68964438d1fe5616a4ada
kianakondo <kianakondo@su.edu.ph>
2025-04-10 23:42:57 +0800

    modifications.

--- knotpedia/src/codes/Sitemap.jsx
+++ knotpedia/src/codes/Sitemap.jsx
@@ -3,49 +3,49 @@ import "./sitemap.css";
 import Navbar from "./Navbar.jsx";
 import Footer from "./Footer.jsx";
 
-const AboutUs = () => {
+const Sitemap = () => {
   return (
     <div className="flex flex-col min-h-screen">
       <Navbar />
-      <div className="POPPINS-ABOUTUS">
+      <div className="POPPINS-sitemap">
         {/* About Section */}
-        <div className="about-header">
-          <h1 className="p-width">KnotPedia Site Map</h1>
+        <div className="sitemap-header">
+          <h1 className="p-width"><b>Knot</b><b>Pedia</b> Site Map</h1>
           <p className="p-width">
           Looking for something specific? Explore our sitemap to easily navigate all our pages.
           </p>
         </div>
 
-        <main className="content-container">
+        <main className="sitemap-content-container">
           {/* Breadcrumb */}
-          <nav className="breadcrumb">
-            <a href="/">Home</a> &gt; About Us
+          <nav className="sitemap-breadcrumb">
+            <a href="/">Home</a> &gt; Site Map
           </nav>
 
           <hr></hr><br></br>
           <h1 style={{ textAlign: "center" }}>Title or something</h1>
           {/* Policy Cards */}
-          <div className="policy-cards">
-            <div className="card" style={{ backgroundColor: "#0d6287" }}>
+          <div className="sitemap-policy-cards">
+            <div className="sitemap-card" style={{ backgroundColor: "#0d6287" }}>
               {/* <div className="icon">
                 <img src="/assets/home-difficulty.png" alt="Difficulty Icon" />
               </div> */}
               <a href="/FAQ">
-                <h4 className="blue title-card">Frequently Asked Questions (FAQs)</h4>
+                <h4 className="sitemap-blue sitemap-title-card">Frequently Asked Questions (FAQs)</h4>
                 <p>Find answers to common questions.</p>
               </a>
             </div>
 
             <div className="card" style={{ backgroundColor: "#5192A5" }}>
               <a href="/TermsAndConditions">
-                <h4 className="blue">Terms & Conditions</h4>
+                <h4 className="sitemap-blue">Terms & Conditions</h4>
                 <p>Understand our site's rules and policies.</p>
               </a>
             </div>
 
-            <div className="card" style={{ backgroundColor: "#b54d49" }}>
+            <div className="sitemap-card" style={{ backgroundColor: "#b54d49" }}>
               <a href="/PrivacyPolicy">
-                <h4 className="red">Privacy Policy</h4>
+                <h4 className="sitemap-red">Privacy Policy</h4>
                 <p>Learn how we handle your data securely.</p>
               </a>
             </div>
@@ -57,4 +57,4 @@ const AboutUs = () => {
   );
 };
 
-export default AboutUs;
+export default Sitemap;
