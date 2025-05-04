830565bba7409baaffc8536bb0a3228855c5c916
kianakondo <kianakondo@su.edu.ph>
2025-05-04 17:15:08 +0800

    minor change

--- knotpedia/src/pages/Terminology/Terminology.jsx
+++ knotpedia/src/pages/Terminology/Terminology.jsx
@@ -5,7 +5,7 @@ import Pagination from "../Components/Pagination";
 import "./Terminology.css";
 import { collection, getDocs } from "firebase/firestore";
 import { db } from "../../firebase";
-import { db } from "../../firebase";
+
 
 const Terminology = () => {
   const [knots, setKnots] = useState([]);
